4c8bb9bee3a1fd3ebaa746b55c40164713647b70
GabrieleLapischies <129321703+GabrieleLapischies@users.noreply.github.com>
2023-04-28 20:19:02 -0300

    Create autentic_usu.php

---
+++ autentic_usu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+< html  lang = " pt " >
+< cabeça >
+    < meta  charset = " UTF-8 " >
+    < meta  name = " viewport "  content = " width=device-width, initial-scale=1.0 " >
+    < title >Página inicial</ title >
+    < link  rel = " folha de estilo "  type = " text/css "   href = " css/style.css " >
+</ cabeça >
+< corpo >
+    < form  id = " inicial "  method = " post "  action = " calculando.php " >
+        < h1 > Calculadora </ h1 >
+        Primeiro Valor: < input  name = " val1 "  type = " text " >< br >
+        Segundo Valor:< input  name = " val2 "  type = " text " >< br >
+        < input  name = " btn "  type = " submit "  value = " + " >
+        < input  name = " btn "  type = " submit "  value = " - " >
+        < input  name = " btn "  type = " submit "  value = " * " >
+        < input  name = " btn "  type = " submit "  value = " / " >< br >
+        < nome da entrada  = " limpar " tipo = " redefinir " valor = " limpar " >  
+    </forma> 
+</ corpo >
+</html> 
